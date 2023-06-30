47108bfcb4298ddfac344bf849509eee68125895
Evgeniy-1998 <125750135+Evgeniy-1998@users.noreply.github.com>
2023-06-30 22:28:12 +0300

    Add files via upload

---
+++ confin.py
@@ -0,0 +1,8 @@
+TOKEN = "6047946774:AAE-onRFVT9mRcuXOIUtIUNlG_O5sCjRGQg"
+keys = {'биткоин': 'BTC',
+        'эфириум': 'ETH',
+
+        'евро': 'EUR',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+        }
